0728fa4d5b62ebca576bf7172940b475fc11639a
Will Beasley <wibeasley@hotmail.com>
2017-06-26 11:15:30 -0500

    allow user to select variable
    during class

--- bohora/01-lesson/server.R
+++ bohora/01-lesson/server.R
@@ -3,7 +3,8 @@ library(shiny)
 shinyServer(function(input, output){
 
   output$distPlot <- renderPlot({
-    x <- faithful[, 2]
+    selected_column_name    <- input$e6
+    x <- faithful[, selected_column_name]
     bins <- seq(min(x), max(x), length.out = input$slider_bin_count + 1)
 
     hist(x, breaks = bins, col = "darkblue", border = "red")
--- bohora/01-lesson/ui.R
+++ bohora/01-lesson/ui.R
@@ -12,11 +12,23 @@ shinyUI(fluidPage(
         min       = 1,
         max       = 50,
         value     = 30
+      ),
+      # I() indicates it is raw JavaScript code that should be evaluated, instead
+      # of a normal character string
+      selectizeInput(
+        inputId    = 'e6',
+        label      = '6. Placeholder',
+        choices    = c("eruptions", "waiting"),
+        # selected   = "eruptions",
+        options    = list(
+          placeholder = 'Please select an option below',
+          onInitialize = I('function() { this.setValue("eruptions"); }')
+        )
       )
-    ),
+    ), #Close the sidebar panel
 
     mainPanel(
       plotOutput("distPlot")
-    )
-  )
-))
+    ) # Close the main panel
+  ) # Close the layout
+)) # Close the page
